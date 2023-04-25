a1f36a5b771c98a603439a8d7759bb90d0c13bd9
Md Raze Alam <mdrazealam7@gmail.com>
2023-04-26 02:20:50 +0530

    my firt

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
+
+/*
+Key:
+5cc37a004b1dc315e7a5769cc7a076a1
+
+Secret:
+7103467cc4945d15
+ */
